914870cf9cfc87da53a2d2d637ab28881991ea34
Verdiyev Zulfugar <39241206+Verdiyev@users.noreply.github.com>
2022-04-06 16:15:53 +0400

    deleting unnecessary comments

--- database.py
+++ database.py
@@ -1,17 +1,6 @@
 import peewee
 from config import settings
-# class PeeweeConnectionState(peewee._ConnectionState):
-#     def __init__(self, **kwargs):
-#         super().__setattr__("_state", peewee.db_state)
-#         super().__init__(**kwargs)
-
-#     def __setattr__(self, name, value):
-#         self._state.get()[name] = value
-
-#     def __getattr__(self, name):
-#         return self._state.get()[name]
 
 
 db = peewee.MySQLDatabase(settings.db_name, user=settings.db_user, password=settings.db_pass,
                          host=settings.db_host, port=settings.db_port)
-# db._state = PeeweeConnectionState()
